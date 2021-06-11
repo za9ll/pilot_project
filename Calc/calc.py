a572ad3a5eefe411c306d5a759dce4183fd117f3
za9ll <85647104+za9ll@users.noreply.github.com>
2021-06-12 01:12:26 +0500

    Update calc.py

--- Calc/calc.py
+++ Calc/calc.py
@@ -1,1 +1,18 @@
+def calc_operation(a, b, operation):
+    if operation == '+':
+        return str(int(a) + int(b));
+    elif operation == '-':
+        return str(int(a) - int(b));
+    elif operation == '*':
+        return str(int(a) * int(b));
+    elif operation == '/':
+        if (b != '0'):
+            return str(int(a) / int(b))
+        else:
+            return "Invalid operation";
 
+
+q=5
+w=4
+e='-'
+print (calc_operation(q,w,e))
