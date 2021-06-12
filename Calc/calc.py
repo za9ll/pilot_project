f8b228fc17de4af60dbb433427651e81df294388
za9ll <85647104+za9ll@users.noreply.github.com>
2021-06-13 02:58:15 +0500

    Update calc.py

--- Calc/calc.py
+++ Calc/calc.py
@@ -1,18 +1,37 @@
-def calc_operation(a, b, operation):
-    if operation == '+':
-        return str(int(a) + int(b));
-    elif operation == '-':
-        return str(int(a) - int(b));
-    elif operation == '*':
-        return str(int(a) * int(b));
-    elif operation == '/':
-        if (b != '0'):
-            return str(int(a) / int(b))
+def division(a, b):
+    if b !=0:
+        return str(int(a / b))
+    else:
+        return 'division by zero'
+
+def chek_data(value):
+    if value != None:
+        return True
+    else:
+        return False
+
+def is_digit(num):
+    if chek_data(num) and isinstance(num, int):
+        return True
+    else:
+        return False
+
+#def chek_operator(trans):
+def count_up(d):
+    if is_digit(d.get('left', None)):
+        if is_digit(d.get('right', None)):
+            if chek_data(d.get('operator', None)):
+                d_operator = { '+': str(d['left'] + d['right']), '-': str(d['left'] - d['right']),'*': str(d['left'] * d['right']),'/': division(d['left'], d['right'])}
+                if chek_data(d_operator.get(d['operator'], None)):
+                    return d_operator[d['operator']]
+                else:
+                    return 'invalid operator'
+            else:
+                return 'invalid operator'
         else:
-            return "Invalid operation";
-
-
-q=66
-w=6
-e='/'
-print (calc_operation(q,w,e))
+            return 'invalid operand "right"'
+    else:
+        return 'invalid operand "left"'
+    
+dic = {'left':1,'right':0,'operator':'/'}
+print (count_up(dic))
