444d1040674cd71be3ecdc3cd036238cf82085be
za9ll <85647104+za9ll@users.noreply.github.com>
2021-06-14 00:17:31 +0500

    Update calc.py

--- Calc/calc.py
+++ Calc/calc.py
@@ -1,8 +1,13 @@
+#!flask/bin/python
+from flask import Flask, request
+
+app = Flask(__name__)
+
 def division(a, b):
     if b !=0:
         return str(int(a / b))
     else:
-        return 'division by zero'
+        return 'division by zero \n'
 
 def chek_data(value):
     if value != None:
@@ -17,21 +22,21 @@ def is_digit(num):
         return False
 
 #def chek_operator(trans):
-def count_up(d):
+@app.route("/count_up", methods=["POST"])
+def count_up():
+    d = request.json    
     if is_digit(d.get('left', None)):
         if is_digit(d.get('right', None)):
             if chek_data(d.get('operator', None)):
                 d_operator = { '+': str(d['left'] + d['right']), '-': str(d['left'] - d['right']),'*': str(d['left'] * d['right']),'/': division(d['left'], d['right'])}
                 if chek_data(d_operator.get(d['operator'], None)):
-                    return d_operator[d['operator']]
+                    return d_operator[d['operator']] + '\n'
                 else:
-                    return 'invalid operator'
+                    return 'invalid operator \n'
             else:
-                return 'invalid operator'
+                return 'invalid operator \n'
         else:
-            return 'invalid operand "right"'
+            return 'invalid operand "right" \n'
     else:
-        return 'invalid operand "left"'
+        return 'invalid operand "left" \n'
     
-dic = {'left':1,'right':0,'operator':'/'}
-print (count_up(dic))
